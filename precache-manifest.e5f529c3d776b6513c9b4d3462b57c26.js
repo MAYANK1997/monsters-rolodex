0457d5fef955d12286e25db68c33541e50689991
MAYANK1997 <mayankpabbi@gmail.com>
2020-05-31 02:00:37 +0530

    Updates

---
+++ precache-manifest.e5f529c3d776b6513c9b4d3462b57c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea112e8cf373bee5258a49c1ffc70f9e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c04af1d247a85dd46c41",
+    "url": "/monsters-rolodex/static/css/main.2c3e5852.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04af1d247a85dd46c41",
+    "url": "/monsters-rolodex/static/js/main.33d3057d.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
